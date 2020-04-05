e6d35c9e912bd24f68f4ef996d2a386124e41e7d
Sasha Abramov <sancha0809@inbox.ru>
2020-04-05 03:12:33 +0300

    Patch 1

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-// const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 
-// const uri = "mongodb+srv://BugiDog:Bugilan080919@cluster0-sfqfz.mongodb.net/test?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-// client.connect(err => {
-//   const collection = client.db("Test");
-//   const myobj = { name: "Company Inc", address: "Highway 37" };
-//   collection.collection("Pazient").insertOne(myobj, function(err, res) {
-//     if (err) throw err;
-//     console.log("1 document inserted");
-//     client.close();
-//   });
-// });
+const uri = "mongodb+srv://BugiDog:Bugilan080919@cluster0-sfqfz.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("Test");
+  const myobj = { name: "Company Inc", address: "Highway 37" };
+  collection.collection("Pazient").insertOne(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    client.close();
+  });
+});
 
 const http = require('http');
 let app = http.createServer((req, res) => {    
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World!\n');
 });
+
 
 app.listen(3000, '127.0.0.1');
 console.log('Node server running on port 3000');
