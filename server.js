7f7e6de5ff8af67854b1681d5682e22bc47c4635
Sasha Abramov <sancha0809@inbox.ru>
2020-04-09 17:35:31 +0300

    Изменили ничего

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const adduser = require('./Routes/adduser')
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const port = process.env.PORT || 3000
+
+
 const app = express() 
 
 app.use(bodyParser.json());
 
 app.use('/', adduser)
  
-app.listen(3000)
+app.listen(port)
