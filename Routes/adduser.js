a40164dd3d12f469495a125d67cdc36ca502bea1
Sasha Abramov <sancha0809@inbox.ru>
2020-04-09 17:22:38 +0300

    Изменили success

--- Routes/adduser.js
+++ Routes/adduser.js
@@ -22,30 +22,54 @@ api.get('/adduser', (req, res) => {
         });
       
         res.send({
-            status: 'sucess', 
+            status: 'success', 
             name: name,
             surName: surName
         })
 })
-
-api.post('/addpsih', (req, res) => {
-    console.log(req.body)   
-
+api.get('/addpsih', (req, res) => {
+    console.log(req.query)  
+    const name=req.query.name   
+    const surName=req.query.surname
+    const pass = req.query.password
   
     const client = new MongoClient(uri, { useNewUrlParser: true });
   
     client.connect(err => {
-            const collection = client.db("Test").collection("Psih");  
-            const myobj = { name: req.body.name, surname : req.body.surname };
-            collection.insertOne(myobj, function(err, res) {
-                if (err) throw err;
-                console.log("1 document inserted");
-                client.close();
-            }); 
-    });
-    res.send({
-        status: 'sucess inserted data', 
-    })
+        const collection = client.db("Test").collection("Psih");  
+        const myobj = { name: name, surname : surName ,password:pass };
+          collection.insertOne(myobj, function(err, res) {
+            if (err) throw err;
+            console.log("1 document inserted");
+            client.close();
+          }); 
+        });
+      
+        res.send({
+            status: 'success', 
+            name: name,
+            surName: surName,
+            password: pass
+        })
 })
+// api.post('/addpsih', (req, res) => {
+//     console.log(req.body)   
+
+  
+//     const client = new MongoClient(uri, { useNewUrlParser: true });
+  
+//     client.connect(err => {
+//             const collection = client.db("Test").collection("Psih");  
+//             const myobj = { name: req.body.name, surname : req.body.surname };
+//             collection.insertOne(myobj, function(err, res) {
+//                 if (err) throw err;
+//                 console.log("1 document inserted");
+//                 client.close();
+//             }); 
+//     });
+//     res.send({
+//         status: 'sucess inserted data', 
+//     })
+// })
 
 module.exports = api;
